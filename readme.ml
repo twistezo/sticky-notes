78d6d18b6019c1ddad85b43f22e76753b7e8a05c
twistezo <luk89@outlook.com>
2017-02-18 19:17:12 +0100

    0.6

---
+++ readme.ml
@@ -0,0 +1,30 @@
+# StickyNotes
+Secured web service for manage notes (Spring + Hibernate + MySQL + Thymeleaf + Bootstrap)
+
+<table>
+    <tr>
+        <td>
+            <img src="http://i.imgur.com/GNY490G.png" width="500">
+        </td>
+        <td>
+            <img src="http://i.imgur.com/Z7EzZtR.png" width="500">
+        </td>
+    </tr>
+    </tr>
+    <tr>
+        <td>
+            <img src="http://i.imgur.com/0DWk564.png" width="500">
+        </td>
+        <td>
+            <img src="http://i.imgur.com/icBlMS3.png" width="500">
+        </td>
+    </tr>
+</table>
+
+
+
+
+
+
+
+</table>
