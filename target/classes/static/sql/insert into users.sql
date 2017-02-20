a9337145254d16087f4b8ef4c2268d5febc36564
twistezo <luk89@outlook.com>
2017-02-20 10:26:28 +0100

    0.9

--- target/classes/static/sql/insert into users.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM stickynotes_db.users;
-
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `password`, `role`, `username`) VALUES
-	(1,	'Adam1',	'Last1',	'$2a$10$zYCGrJGBYRJlAoHLNTTJZ.8Gw5pq0gvZFpXHOUz5axZx6.iUjjiNi',	"ROLE_USER",	'user1'),
-    (2,	'Adam2',	'Last2',	'$2a$10$zYCGrJGBYRJlAoHLNTTJZ.8Gw5pq0gvZFpXHOUz5axZx6.iUjjiNi',	"ROLE_USER",	'user2'),
-    (3,	'Adam3',	'Last3',	'$2a$10$zYCGrJGBYRJlAoHLNTTJZ.8Gw5pq0gvZFpXHOUz5axZx6.iUjjiNi',	"ROLE_USER",	'user3'),
-    (4,	'Adam4',	'Last4',	'$2a$10$zYCGrJGBYRJlAoHLNTTJZ.8Gw5pq0gvZFpXHOUz5axZx6.iUjjiNi',	"ROLE_USER",	'user4'),
-    (5,	'Adam5',	'Last5',	'$2a$10$zYCGrJGBYRJlAoHLNTTJZ.8Gw5pq0gvZFpXHOUz5axZx6.iUjjiNi',	"ROLE_USER",	'user5');
