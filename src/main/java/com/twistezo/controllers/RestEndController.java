68c60e96206cb39534fa471494a536c95d42ffa4
twistezo <luk89@outlook.com>
2017-04-23 15:58:10 +0200

    add rest controller

---
+++ src/main/java/com/twistezo/controllers/RestEndController.java
@@ -0,0 +1,45 @@
+package com.twistezo.controllers;
+
+import com.twistezo.models.Note;
+import com.twistezo.models.User;
+import com.twistezo.services.NoteService;
+import com.twistezo.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/**
+ * @author twistezo (26.03.2017)
+ */
+
+@RestController
+public class RestEndController {
+    private NoteService noteService;
+    private UserService userService;
+
+    public RestEndController(NoteService noteService, UserService userService) {
+        this.noteService = noteService;
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "rest/notes/{id}", method = RequestMethod.GET)
+    public Note getNoteById (@PathVariable("id") Long id) {
+        return noteService.findById(id);
+    }
+
+    @RequestMapping(value = "rest/notes", method = RequestMethod.GET)
+    public List<Note> getAllNotes () {
+        return noteService.findAllByOrderByDate();
+    }
+
+    @RequestMapping(value = "rest/users/{id}", method = RequestMethod.GET)
+    public User getUserById (@PathVariable("id") Long id) {
+        return userService.findById(id);
+    }
+
+    @RequestMapping(value = "rest/users", method = RequestMethod.GET)
+    public List<User> getAllUsers () {
+        return userService.findAll();
+    }
+}
